50fde853c879b28dd545706d61c86a28285f3ab3
Henry Balen <hcb2007@mac.com>
2013-11-01 20:01:22 -0400

    Convert to gem for Rails 3.2 and greater

--- lib/active_record/acts/enumerated.rb
+++ lib/active_record/acts/enumerated.rb
@@ -2,19 +2,23 @@
 # Released under the MIT License.  See the LICENSE file for more details.
 
 module ActiveRecord
-  module Acts
-    module Enumerated
+  module Acts 
+    module Enumerated 
       def self.append_features(base)
-        super
-        base.extend(MacroMethods)
+        super        
+        base.extend(MacroMethods)              
       end
       
-      module MacroMethods
+      module MacroMethods          
         def acts_as_enumerated(options = {})
           valid_keys = [:conditions, :order, :on_lookup_failure]
           options.assert_valid_keys(*valid_keys)
-          valid_keys.each do |key|
-            write_inheritable_attribute("acts_enumerated_#{key.to_s}".to_sym, options[key]) if options.has_key? key
+          class_attribute :acts_enumerated_on_lookup_failure           
+          valid_keys.each do |key|   
+            class_attribute "acts_enumerated_#{key.to_s}"
+            if options.has_key?( key )
+              self.send "acts_enumerated_#{key.to_s}=", options[key]
+            end            
           end
           
           unless self.is_a? ActiveRecord::Acts::Enumerated::ClassMethods
@@ -29,15 +33,14 @@ module ActiveRecord
         end
       end
       
-      module ClassMethods
+      module ClassMethods  
         attr_accessor :enumeration_model_updates_permitted
         
         def all
           return @all if @all
-          @all = find(:all,
-                      :conditions => read_inheritable_attribute(:acts_enumerated_conditions),
-                      :order => read_inheritable_attribute(:acts_enumerated_order)
-                      ).collect{|val| val.freeze}.freeze
+          conditions = self.acts_enumerated_conditions
+          order = self.acts_enumerated_order
+          @all = where(conditions).order(order).collect{|val| val.freeze}.freeze          
         end
 
         def [](arg)
@@ -49,11 +52,11 @@ module ActiveRecord
           when Fixnum
             rval = lookup_id(arg) and return rval
           when nil
-            rval = nil
+            rval = nil 
           else
-            raise TypeError, "#{self.name}[]: argument should be a String, Symbol or Fixnum but got a: #{arg.class.name}"
+            raise TypeError, "#{self.name}[]: argument should be a String, Symbol or Fixnum but got a: #{arg.class.name}"            
           end
-          self.send((read_inheritable_attribute(:acts_enumerated_on_lookup_failure) || :enforce_strict_literals), arg)
+          self.send(:acts_enumerated_on_lookup_failure) || self.send(:enforce_strict_literals, arg)
         end
 
         def lookup_id(arg)
@@ -63,7 +66,7 @@ module ActiveRecord
         def lookup_name(arg)
           all_by_name[arg]
         end
-
+                                   
         def include?(arg)
           case arg
           when Symbol
@@ -73,17 +76,17 @@ module ActiveRecord
           when Fixnum
             return !lookup_id(arg).nil?
           when self
-            possible_match = lookup_id(arg.id)
+            possible_match = lookup_id(arg.id) 
             return !possible_match.nil? && possible_match == arg
           end
           return false
         end
 
         # NOTE: purging the cache is sort of pointless because
-        # of the per-process rails model.
-        # By default this blows up noisily just in case you try to be more
-        # clever than rails allows.
-        # For those times (like in Migrations) when you really do want to
+        # of the per-process rails model.  
+        # By default this blows up noisily just in case you try to be more 
+        # clever than rails allows.  
+        # For those times (like in Migrations) when you really do want to 
         # alter the records you can silence the carping by setting
         # enumeration_model_updates_permitted to true.
         def purge_enumerations_cache
@@ -93,25 +96,25 @@ module ActiveRecord
           @all = @all_by_name = @all_by_id = nil
         end
 
-        private
-
-        def all_by_id
+        private 
+        
+        def all_by_id 
           return @all_by_id if @all_by_id
-          @all_by_id = all.inject({}) { |memo, item| memo[item.id] = item; memo;}.freeze
+          @all_by_id = all.inject({}) { |memo, item| memo[item.id] = item; memo;}.freeze              
         end
         
         def all_by_name
           return @all_by_name if @all_by_name
           begin
-            @all_by_name = all.inject({}) { |memo, item| memo[item.name] = item; memo;}.freeze
+            @all_by_name = all.inject({}) { |memo, item| memo[item.name] = item; memo;}.freeze              
           rescue NoMethodError => err
             if err.name == :name
               raise TypeError, "#{self.name}: you need to define a 'name' column in the table '#{table_name}'"
             end
             raise
-          end
-        end
-
+          end            
+        end   
+        
         def enforce_none(arg)
           return nil
         end
@@ -126,7 +129,7 @@ module ActiveRecord
           end
           return nil
         end
-
+        
       end
 
       module InstanceMethods
@@ -155,22 +158,23 @@ module ActiveRecord
 
         private
 
-        # NOTE: updating the models that back an acts_as_enumerated is
+        # NOTE: updating the models that back an acts_as_enumerated is 
         # rather dangerous because of rails' per-process model.
         # The cached values could get out of synch between processes
-        # and rather than completely disallow changes I make you jump
-        # through an extra hoop just in case you're defining your enumeration
+        # and rather than completely disallow changes I make you jump 
+        # through an extra hoop just in case you're defining your enumeration 
         # values in Migrations.  I.e. set enumeration_model_updates_permitted = true
         def enumeration_model_update
-          if self.class.enumeration_model_updates_permitted
+          if self.class.enumeration_model_updates_permitted    
             self.class.purge_enumerations_cache
             return true
           end
           # Ugh.  This just seems hack-ish.  I wonder if there's a better way.
-          self.errors.add('name', "changes to acts_as_enumeration model instances are not permitted")
+          self.errors.add('name', "changes to acts_as_enumeration model instances are not permitted")   
           return false
         end
       end
     end
   end
 end
+        
